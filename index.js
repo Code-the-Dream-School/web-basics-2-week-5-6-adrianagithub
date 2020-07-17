3c67305620fd65ddfd598127f8f83d01a9e5c8f5
Adriana <adi_111@icloud.com>
2020-07-17 14:11:55 -0700

    Last update

--- index.js
+++ index.js
@@ -10,6 +10,103 @@
 //in the index.html file you are going to find 4 more ids: 'name_player1' , 'name_player2' , 'ships_player1' , 'ships_player2'. We want to see the information of each player in the respective elements
 //As our previous Battleship, the winner is the player that hits the 4 opponent's ships first
 //one more Thing create a 'reset' and a 'new game' buttons as childs of the element with the id 'buttons'. the reset button has to start the game again and the new game create a new game with new players and a new random board.
+var player1 = {
+  name : '',
+  shipCount: 4,
+  gameBoard: [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]
+};
+var player2 = {
+  name : '',
+  shipCount: 4,
+  gameBoard: [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ]
+};
+
+var x;
+var y;
+var currentplayer;
+var opponent;
+var coordp1 = [];
+var coordp2 = [];
+function getRandomNumber() {
+  var valmin=0; 
+  var valmax=4;  
+  var num = Math.floor(Math.random() * (+valmax - +valmin)) + +valmin; 
+  return num;
+}
+
+function createShip(player, ships) {
+  let i = 0;
+  do {
+    const x = getRandomNumber();
+    const y = getRandomNumber();
+    if (player.gameBoard[x][y] !== 1) {
+      player.gameBoard[x][y] = 1;
+      i++;
+    }
+  } while (i < ships)
+}
+//console.log("Player 1 " + JSON.stringify(player1.gameBoard));
+//console.log("Player 2 " + JSON.stringify(player2.gameBoard));
+
+
+createShip(player1, 4)
+//console.log("Player 1 " + JSON.stringify(player1.gameBoard));
+console.log("Player 1 " + player1.gameBoard);
+createShip(player2, 4)
+console.log("Player 2 " + player2.gameBoard);
+//console.log("Player 2 " + JSON.stringify(player2.gameBoard));
+
+function disparar(player,x,y){
+  if (player.gameBoard[x][y] === 1){
+    console.log("estas disparando")
+    console.log(player)
+    //alert ("You hit your opponent has one less ship")
+    player.gameBoard[x][y] = 0
+    player.shipCount --
+    // if (currentplayer === player1){//***cambiar turno */
+    //   currentplayer = player2;
+    // } else{
+    //   currentplayer = player1;
+    // }
+  }else{
+    console.log ("lo siento no estas hundiendo barcos de tu oponente")
+    //alert("Sorry you did not hit your opponent")
+    console.log(player)
+    // if (currentplayer === player1){//***cambiar turno */
+    //   currentplayer = player2;
+    // } else{
+    //   currentplayer = player1;
+    // }
+  }
+  return player.name;
+}
+
+//function reset() { //DO NOT USE AS WE DO NOT HAVE A FORM HERE
+//  document.getElementById("myForm").reset();
+//CREATE BUTTONS
+const NodeButt = document.getElementById('buttons');
+var buttonRes = document.createElement("button");
+buttonRes.innerHTML = "Reset Game";
+var buttonNew = document.createElement("button");
+buttonNew.innerHTML = "New Game";
+NodeButt.appendChild(buttonRes);
+NodeButt.appendChild(buttonNew);
+buttonRes.addEventListener("click", refreshPage);
+buttonNew.addEventListener("click", refreshPage);
+function refreshPage(){
+  window.location.reload();
+} 
+
 
 const board_Player1 = document.getElementById('board_player1');
 
@@ -29,10 +126,50 @@ for (var x = 0; x < 4; x++) {
           console.log( cell.textContent) //display the coordinates in the console
           cell.style.visibility = 'hidden';// this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
           //cell.style.background ="purple"; //with this propertie you can change the background color of the clicked cell. try comment the line bellow and uncomment this line. Do not forget to save this file and refresh the borwser to see the changes
+          let coordp1 = cell.textContent.split(',')
+          console.log(coordp1,"array")
+          //cell.textContent.split(',')
+          var x = parseInt(coordp1[0])
+          var y = parseInt(coordp1[1])
+          console.log (x)
+          console.log (y)
       });
 
       li.appendChild(cell); //adding each cell into the row number x
     }
 
      board_Player1.appendChild(li); //adding each row into the board
+}
+
+const board_player2 = document.getElementById('board_player2');
+
+for (var x = 0; x < 4; x++) {
+
+    const li = document.createElement('li'); // creating childs for the list (board), in this case represent a row number 'x' of the board
+
+    for (var y = 0; y < 4; y++) {
+      const cell = document.createElement('div');
+      cell.className = "square"; // adding css properties to make it looks like a square
+      cell.textContent = `${x},${y}`;  // saves the coordinates as a string value 'x,y'
+      cell.value = 0;//state of the cell
+
+      //this function adds the click event to each cell
+      cell.addEventListener( 'click', (e) => {
+          let cell = e.target; // get the element clicked
+          console.log( cell.textContent) //display the coordinates in the console
+          cell.style.visibility = 'hidden';// this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
+          //cell.style.background ="purple"; //with this propertie you can change the background color of the clicked cell. try comment the line bellow and uncomment this line. Do not forget to save this file and refresh the borwser to see the changes
+          let coordp2 = cell.textContent.split(',')
+          console.log(coordp2,"array")
+          //cell.textContent.split(',')
+          var x = parseInt(coordp2[0])
+          var y = parseInt(coordp2[1])
+          console.log (x)
+          console.log (y)
+      });
+
+      li.appendChild(cell); //adding each cell into the row number x
+    }
+
+     board_player2.appendChild(li); //adding each row into the board
 }

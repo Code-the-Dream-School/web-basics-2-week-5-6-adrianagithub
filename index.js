c4d5fc94d294487d98b97cc388222955bc47d44c
Adriana <adi_111@icloud.com>
2020-07-29 23:26:53 -0700

    Last version using functions to accomplish DRY and objects

--- index.js
+++ index.js
@@ -11,26 +11,28 @@
 //one more Thing create a 'reset' and a 'new game' buttons as childs of the element with the id 'buttons'. the reset button has to start the game again and the new game create a new game with new players and a new random board.
 //***OBJECTS***//
 var player1 = {
-  name : '',
+  name: "",
   shipCount: 4,
   gameBoard: [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
-    [0, 0, 0, 0]
-  ]
+    [0, 0, 0, 0],
+  ],
 };
 var player2 = {
-  name : '',
+  name: "",
   shipCount: 4,
   gameBoard: [
     [0, 0, 0, 0],
     [0, 0, 0, 0],
     [0, 0, 0, 0],
-    [0, 0, 0, 0]
-  ]
+    [0, 0, 0, 0],
+  ],
 };
 //***VARIABLES***//
+const board_Player1 = document.getElementById("board_player1");
+const board_Player2 = document.getElementById("board_player2");
 var x;
 var y;
 var coordp1 = [];
@@ -38,12 +40,11 @@ var coordp2 = [];
 var currentplayer = player1;
 var opponent = player2; //INIT currentplayer
 var player;
-var lives;
 //***FUNCTIONS***//
 function getRandomNumber() {
-  var valmin=0; 
-  var valmax=4;  
-  var num = Math.floor(Math.random() * (+valmax - +valmin)) + +valmin; 
+  var valmin = 0;
+  var valmax = 4;
+  var num = Math.floor(Math.random() * (+valmax - +valmin)) + +valmin;
   return num;
 }
 function createShip(player, ships) {
@@ -55,145 +56,131 @@ function createShip(player, ships) {
       player.gameBoard[x][y] = 1;
       i++;
     }
-  } while (i < ships)
+  } while (i < ships);
 }
-function disparar(player,x,y){
-  if (player.gameBoard[x][y] === 1){
-    alert ("You hit your opponent has one less ship")
-    player.gameBoard[x][y] = 0
-    player.shipCount --
-    }
-  else {
-    alert("Sorry you did not hit your opponent")
-    console.log(player)
+function disparar(player, x, y) {
+  if (player.gameBoard[x][y] === 1) {
+    alert("You hit your opponent has one less ship");
+    player.gameBoard[x][y] = 0;
+    player.shipCount--;
+  } else {
+    alert("Sorry you did not hit your opponent");
+    console.log(player);
   }
   return player.name;
 }
-function createButtons () {
-    const NodeButt = document.getElementById('buttons');
-    var buttonRes = document.createElement("button");
-    buttonRes.innerHTML = "Reset Game";
-    var buttonNew = document.createElement("button");
-    buttonNew.innerHTML = "New Game";
-    NodeButt.appendChild(buttonRes);
-    NodeButt.appendChild(buttonNew);
-    buttonRes.addEventListener("click", refreshPage);
-    buttonNew.addEventListener("click", refreshPage);
+function createButtons() {
+  const NodeButt = document.getElementById("buttons");
+  var buttonRes = document.createElement("button");
+  buttonRes.innerHTML = "Reset Game";
+  var buttonNew = document.createElement("button");
+  buttonNew.innerHTML = "New Game";
+  NodeButt.appendChild(buttonRes);
+  NodeButt.appendChild(buttonNew);
+  buttonRes.addEventListener("click", reset);
+  buttonNew.addEventListener("click", refreshPage);
   return;
 }
-//reset wont apply function refreshPage
-//new game
-function refreshPage(){
+function refreshPage() {
   window.location.reload();
-} 
-
-const board_Player1 = document.getElementById('board_player1');
-for (var x = 0; x < 4; x++) {
-    const li = document.createElement('li'); // creating childs for the list (board), in this case represent a row number 'x' of the board
-    for (var y = 0; y < 4; y++) {
-      const cell = document.createElement('div');
-      cell.className = "square"; // adding css properties to make it looks like a square
-      cell.textContent = `${x},${y}`;  // saves the coordinates as a string value 'x,y'
-      cell.value = 0;//state of the cell
-      console.log (currentplayer)
-      console.log (opponent)
-      //this function adds the click event to each cell
-      cell.addEventListener( 'click', (e) => {
-          if (document.getElementById("board_player2").disabled === true){
-            return false;
-          }
-          document.getElementById("board_player2").disabled = true;
-          document.getElementById("board_player1").disabled = false;
-          let cell = e.target; // get the element clicked
-          console.log( cell.textContent) //display the coordinates in the console
-          cell.style.visibility = 'hidden';// this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
-          //cell.style.background ="purple"; //with this propertie you can change the background color of the clicked cell. try comment the line bellow and uncomment this line. Do not forget to save this file and refresh the borwser to see the changes
-          let coordp1 = cell.textContent.split(',')
-          //console.log(coordp1,"array")
-          var x = parseInt(coordp1[0])
-          var y = parseInt(coordp1[1])
-          disparar(player1,x,y)// player2 shot player 1 in player1 board
-          console.log (player1.shipCount)
-          currentplayer = player1; //switch turn to shot to player 1
-          opponent = player2;
-          player = currentplayer.name;
-          lives = currentplayer.shipCount;        
-          document.getElementById("turn_player").textContent = player;//AQUI
-          document.getElementById("ships_player1").textContent = livesp1;
-          document.getElementById("ships_player2").textContent = livesp2;
-          
-          if ((player1.shipCount === 0) || (player2.shipCount === 0)){//END THE GAME AND REFRESH THE PAGE
-            alert ("I am sorry you loose all your ships the GAME IS OVER");
-            refreshPage()
-          }
-      });
-      li.appendChild(cell); //adding each cell into the row number x
-    }
-
-     board_Player1.appendChild(li); //adding each row into the board
+}
+function reset() {
+  board_Player1.innerHTML = "";
+  board_Player2.innerHTML = "";
+  player1.shipCount = 4;
+  player2.shipCount = 4;
+  player1.gameBoard = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+  player2.gameBoard = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+  currentplayer = player1;
+  opponent = player2; //INIT currentplayer
+  createShip(player1, 4);
+  createShip(player2, 4);
+  boards(board_Player1);
+  boards(board_Player2);
+  document.getElementById("ships_player1").textContent = player1.shipCount;
+  document.getElementById("ships_player2").textContent = player2.shipCount;
+  document.getElementById("turn_player").textContent = currentplayer.name;
+}
+function gameWinner() {
+  var winner = player1.shipCount === 0 ? player2.name : player1.name;
+  alert("The winner of this game is PLAYER " + winner + " THE GAME IS OVER");
+  return winner;
 }
 
-const board_player2 = document.getElementById('board_player2');
-for (var x = 0; x < 4; x++) {
-    const li = document.createElement('li'); // creating childs for the list (board), in this case represent a row number 'x' of the board
+function boards(playerBoard) {
+  //const board_Player1 = document.getElementById('board_player1');
+  for (var x = 0; x < 4; x++) {
+    const li = document.createElement("li"); // creating childs for the list (board), in this case represent a row number 'x' of the board
     for (var y = 0; y < 4; y++) {
-      const cell = document.createElement('div');
+      const cell = document.createElement("div");
       cell.className = "square"; // adding css properties to make it looks like a square
-      cell.textContent = `${x},${y}`;  // saves the coordinates as a string value 'x,y'
-      cell.value = 0;//state of the cell
+      cell.textContent = `${x},${y}`; // saves the coordinates as a string value 'x,y'
+      cell.value = 0; //state of the cell
       //this function adds the click event to each cell
-      cell.addEventListener( 'click', (e) => {
-        if (document.getElementById("board_player1").disabled === true){
-          return false;
-        }
-        document.getElementById("board_player1").disabled = true;
-        document.getElementById("board_player2").disabled = false;
+      cell.addEventListener("click", (e) => {
+        //Retrieve the square that was clicked
         let cell = e.target; // get the element clicked
-        console.log( cell.textContent) //display the coordinates in the console
-        cell.style.visibility = 'hidden';// this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
+        //Check if the square belongs to our opponents board
+        let boardElement = cell.parentNode.parentNode;
+        if (currentplayer == player1 && boardElement == board_Player1) {
+          return;
+        } else if (currentplayer == player2 && boardElement == board_Player2) {
+          return;
+        }
+        cell.style.visibility = "hidden"; // this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
+        //Check if the square is the ship
+        let coord = cell.textContent.split(",");
+        //console.log(coordp1,"array")
+        var x = parseInt(coord[0]);
+        var y = parseInt(coord[1]);
+        disparar(opponent, x, y); // player2 shot player 1 in player1 board
+        //Check if the current player has won
+        if (player1.shipCount === 0 || player2.shipCount === 0) {
+          //END THE GAME AND REFRESH THE PAGE
+          alert("I am sorry you loose all your ships the GAME IS OVER");
+          gameWinner();
+          refreshPage();
+        }
+        //Change the current player to be the opponent
+        currentplayer = opponent;
+        opponent = currentplayer == player1 ? player2 : player1;
+        document.getElementById("turn_player").textContent = currentplayer.name;
+        console.log(cell.textContent); //display the coordinates in the console
         //cell.style.background ="purple"; //with this propertie you can change the background color of the clicked cell. try comment the line bellow and uncomment this line. Do not forget to save this file and refresh the borwser to see the changes
-        let coordp2 = cell.textContent.split(',')
-        console.log(coordp2,"array")
-        var x = parseInt(coordp2[0])
-        var y = parseInt(coordp2[1])
-        disparar(player2,x,y)//player 1 shot player2 in her board
-        currentplayer = player2;//CAMBIA TURNO
-        opponent = player1;
-        player = currentplayer.name;
         livesp1 = player1.shipCount;
         livesp2 = player2.shipCount;
-        document.getElementById("turn_player").textContent = player;
+        //
         document.getElementById("ships_player1").textContent = livesp1;
         document.getElementById("ships_player2").textContent = livesp2;
-        if ((player1.shipCount === 0) || (player2.shipCount === 0)){
-            alert ("I am sorry you loose all your ships the GAME IS OVER");
-            refreshPage();
-        }
       });
       li.appendChild(cell); //adding each cell into the row number x
     }
-     board_player2.appendChild(li); //adding each row into the board
-}
-
+    playerBoard.appendChild(li); //adding each row into the board
+  }
+} //end function boards
 createButtons();
-createShip(player1, 4)
-//console.log("Player 1 " + JSON.stringify(player1.gameBoard));
+createShip(player1, 4);
 console.log("Player 1 " + player1.gameBoard);
-createShip(player2, 4)
+createShip(player2, 4);
 console.log("Player 2 " + player2.gameBoard);
-//console.log("Player 2 " + JSON.stringify(player2.gameBoard));
-
-
-setTimeout(function(){ 
-  player1.name = prompt ('Welcome, what is your name?')
-  player2.name = prompt ('Welcome, what is your name?')
-  alert ("We will play the Battleship Game")
-  alert ("You will have one turn at the time")
-});
-
-
-
-
-
-
-/// FALTA WINNER 
+boards(board_Player1);
+boards(board_Player2);
+player1.name = prompt("Welcome, what is your name?");
+player2.name = prompt("Welcome, what is your name?");
+alert("We will play the Battleship Game");
+alert("You will have one turn at the time");
+document.getElementById("ships_player1").textContent = player1.shipCount;
+document.getElementById("ships_player2").textContent = player2.shipCount;
+document.getElementById("name_player1").textContent = player1.name;
+document.getElementById("name_player2").textContent = player2.name;
+document.getElementById("turn_player").textContent = currentplayer.name;
